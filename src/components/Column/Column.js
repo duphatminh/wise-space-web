47c707401c72ce40caee0dbc0591f3934fddbe1b
duphatminh <duphatminhh@gmail.com>
2022-11-15 00:48:40 +0700

    initital_sample_data

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,25 +1,22 @@
 import React from "react";
 
 import './Column.scss'
-import Task from "components/Task/Task";
+import Card from "components/Card/Card";
+import { mapOrder } from "utilities/sorts";
 
-function Column() {
-    return (
-        <div className='columns'>
-          <header>DOING DOING</header>
-          <ul className="task-list">
-            <Task/>
-            <li className="task-item">Tao sẽ thử mày</li>
-            <li className="task-item">Tao sẽ thử mày</li>
-            <li className="task-item">Tao sẽ thử mày</li>
-            <li className="task-item">Tao sẽ thử mày</li> 
-            <li className="task-item">Tao sẽ thử mày</li>
-            <li className="task-item">Tao sẽ thử mày</li>
-            <li className="task-item">Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-    )
+function Column(props) {
+  const { column } = props
+  const cards = mapOrder(column.cards, column.cardOrder, 'id')
+  return (
+    <div className='columns'>
+      
+      <header>{ column.title }</header>
+      <ul className="card-list">
+        {cards.map((card, index) => <Card key={index} card={card}/>)}
+      </ul>
+      <footer>Add another Card</footer>
+    </div>
+  )
 }
 
 export default Column

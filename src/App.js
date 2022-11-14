fd2ca59000d189013e315b5704b9e5ab012f5f44
duphatminh <duphatminhh@gmail.com>
2022-11-14 21:40:47 +0700

    Clean React Architecture

--- src/App.js
+++ src/App.js
@@ -1,114 +1,18 @@
+import React from "react";
 import './App.scss';
+
+import Appbar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
+
 
 function App() {
   return (
     <div className="wise-space-master">
-      <nav className='navbar app'>App Bar</nav>
-      <nav className='navbar board'>Board Bar</nav>
-      <div className='board-columns'>
-        <div className='columns'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
-              TILTLE: GROUP 3
-            </li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li> 
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-        <div className='columns'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
-              TILTLE: GROUP 3
-            </li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li> 
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-        <div className='columns'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
-              TILTLE: GROUP 3
-            </li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li> 
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-        <div className='columns'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
-              TILTLE: GROUP 3
-            </li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li> 
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-        <div className='columns'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
-              TILTLE: GROUP 3
-            </li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li> 
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-        <div className='columns'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
-              TILTLE: GROUP 3
-            </li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li> 
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-            <li>Tao sẽ thử mày</li>
-          </ul>
-          <footer>Add another Card</footer>
-        </div>
-      </div>
+      <Appbar/>
+      <BoardBar/>
+      <BoardContent/>
+      
     </div>
   );
 }
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './AppBar.scss'
+
+function Appbar() {
+    return (
+        <nav className='navbar-app'>App Bar</nav>
+    )
+}
+
+export default Appbar

5be47f8087768a606162c83e717b0222ad4be44a
duphatminh <duphatminhh@gmail.com>
2022-11-14 18:49:33 +0700

    Design Wise Space Front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,114 @@
-import './App.js';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="wise-space-master">
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar board'>Board Bar</nav>
+      <div className='board-columns'>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
+              TILTLE: GROUP 3
+            </li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li> 
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+          </ul>
+          <footer>Add another Card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
+              TILTLE: GROUP 3
+            </li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li> 
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+          </ul>
+          <footer>Add another Card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
+              TILTLE: GROUP 3
+            </li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li> 
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+          </ul>
+          <footer>Add another Card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
+              TILTLE: GROUP 3
+            </li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li> 
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+          </ul>
+          <footer>Add another Card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
+              TILTLE: GROUP 3
+            </li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li> 
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+          </ul>
+          <footer>Add another Card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://thidaihoc.vn/wp-content/uploads/2022/03/vanlang-online-truong-dai-hoc-van-lang.jpg" alt="ảnh để test thôi"/>
+              TILTLE: GROUP 3
+            </li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li> 
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+            <li>Tao sẽ thử mày</li>
+          </ul>
+          <footer>Add another Card</footer>
+        </div>
+      </div>
     </div>
   );
 }

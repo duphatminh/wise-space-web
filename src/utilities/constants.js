896506104c79accb89527be8173cc49eebe92c9c
duphatminh <duphatminhh@gmail.com>
2022-11-23 00:44:01 +0700

    Deploy Wise Space

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,6 @@
 
-export const API_ROOT = 'http://localhost:7722'
+// export const API_ROOT = 'http://localhost:7722'
+export const API_ROOT = 'https://wise-space.herokuapp.com'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
